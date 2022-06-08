10da66c6ebeb1280d93030b2cc91328ab9694280
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-06-08 15:25:24 +0700

    Add project files.

---
+++ Connected/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string
+        }
+    }
+}
